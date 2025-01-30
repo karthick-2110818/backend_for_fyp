f2fc84394b21f16ede6c6ce0d17a1ada8b816f30
karthick-2110818 <karthick2110818@ssn.edu.in>
2025-01-30 11:41:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = 3000;
+const port = 10000;
 
 // Middleware to parse JSON data
 app.use(bodyParser.json());
